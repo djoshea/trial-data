6dab1f00aac84af96a71a0d9222967975e24216e
Dan O'Shea <dan@djoshea.com>
2016-01-03 17:17:40 -0500

    Filtering improvements, filtfilt

--- +TrialDataUtilities/+Data/filterIgnoreLeadingTrailingNaNs.m
+++ +TrialDataUtilities/+Data/filterIgnoreLeadingTrailingNaNs.m
@@ -1,26 +1,39 @@
-function mat = filterIgnoreLeadingTrailingNaNs(B, A, mat, varargin)
+function out = filterIgnoreLeadingTrailingNaNs(B, A, mat, varargin)
     % matFilt = filterIgnoreLeadingTrailingNaNs(B, A, mat)
     
     p = inputParser;
+    p.addParameter('dim', [], @(x) isempty(x) || isscalar(x));
     % if true subtracts first sample from each signal which can help reduce onset transients for IIR filters
     p.addParameter('subtractFirstSample', false, @islogical);
     % if true, the first sample will be added back on afterwards
     p.addParameter('addBackFirstSample', false, @islogical);
+    p.addParameter('filtfilt', false, @islogical);
     p.parse(varargin{:});
     
-    if ~any(isnan(mat(:)))
-        firstSample = mat(:, 1);
-        if p.Results.subtractFirstSample
-            mat = mat - firstSample;
-        end
-        mat = filter(mat, B, A);
-        if p.Results.subtractFirstSample && p.Results.addBackFirstSample
-            mat = mat + firstSample;
-        end
+    if iscell(mat)
+        out = cellfun(@filterMat, mat, 'UniformOutput', false);
     else
-        % for each column, filter from the first to last non-NaN ind
+        out = filterMat(mat);
+    end
+
+    function mat = filterMat(mat)
+        dim = p.Results.dim;
+        if isempty(dim)
+            % auto choose first non singular dimension
+            dim = TensorUtils.firstNonSingletonDim(mat);
+        else
+            dim = 1;
+        end
+        
+        if dim ~= 1
+            % place dim at second dimension slot
+            mat = shiftdim(mat, dim - 1);
+        end
+        
         nCol = size(mat(:, :), 2);
-        emptyCol = zeros(size(mat, 1), 1);
+       
+        % for each column, filter from the first to last non-NaN ind
+        emptyCol = nan(size(mat, 1), 1);
         for iC = 1:nCol
             thisCol = mat(:, iC);
             filtCol = emptyCol;
@@ -31,11 +44,20 @@ function mat = filterIgnoreLeadingTrailingNaNs(B, A, mat, varargin)
                 firstSample = thisCol(start);
                 thisCol = thisCol - firstSample;
             end
-            filtCol(start:stop) = filter(B, A, thisCol(start:stop));
+            if p.Results.filtfilt
+                filtCol(start:stop) = filtfilt(B, A, thisCol(start:stop));
+            else
+                filtCol(start:stop) = filter(B, A, thisCol(start:stop));
+            end
             if p.Results.subtractFirstSample && p.Results.addBackFirstSample && ~isempty(start)
                 filtCol = filtCol + firstSample;
             end
             mat(:, iC) = filtCol;
+        end
+
+        if dim ~= 1
+            % place dim at second dimension slot
+            mat = shiftdim(mat, 1 - dim);
         end
     end
 end
