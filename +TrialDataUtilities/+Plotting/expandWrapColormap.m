9f10c56cbd8b12bcc4fb5afcc44c3859b8d10981
Dan O'Shea <dan@djoshea.com>
2016-05-04 10:49:05 -0700

    Adding stairs with conf interval

--- +TrialDataUtilities/+Plotting/expandWrapColormap.m
+++ +TrialDataUtilities/+Plotting/expandWrapColormap.m
@@ -1,10 +1,13 @@
 function map = expandWrapColormap(map, n)
     if isa(map, 'function_handle')
         map = map(n);
-    elseif size(map, 1) < n
-        % make at least big enough
-        map = repmat(map, ceil(n / size(map, 1)), 1);
-%         % cut it down to size - actually no need
-%         map = map(1:n, :);
+    else
+        map = TrialDataUtilities.Plotting.convertColorsToMatrix(map);
+        if size(map, 1) < n
+            % make at least big enough
+            map = repmat(map, ceil(n / size(map, 1)), 1);
+            % cut it down to size (typically not necessary)
+            map = map(1:n, :);
+        end
     end
 end
--- +TrialDataUtilities/+Plotting/stairsError.m
+++ +TrialDataUtilities/+Plotting/stairsError.m
@@ -3,6 +3,10 @@ function [hMean, hError] = stairsError(x,y, varargin)
 % stairsError(x, y, yerror, ...) - plot +/- yerror 
 % stairsError(x, y, ylo, yhi, ...) - plot confidence intervals
 %
+% for stairsError, the last value of x can be specified manually using
+% parameter: 'lastX', otherwise it will be picked to match x(end)+(x(end)-x(end-1))
+%
+%
 % params:
 %   color - width of stairs line, defaults to next color in ColorOrder
 %   line width 
@@ -10,22 +14,53 @@ function [hMean, hError] = stairsError(x,y, varargin)
 %   errorAlpha - defaults to 0.8
 
     p = inputParser();
-    p.addRequired('err1', [], @ismatrix);
+    p.addRequired('err1', @ismatrix);
     p.addOptional('err2', [], @ismatrix);
     p.addParameter('color', [], @(x) true);
+    p.addParameter('errorStyle', 'fill', @ischar); % fill or stairs
     p.addParameter('errorColor', [], @(x) true);
-    p.addParameter('errorAlpha', 0.8, @isscalar);
-    p.addParamValue('lineWidth', 1, @isscalar);
+    p.addParameter('errorAlpha', 0.5, @isscalar);
+    p.addParameter('lineWidth', 1, @isscalar);
+    p.addParameter('lastX', [], @isvector);
     p.KeepUnmatched = true;
     p.CaseSensitive = false;
     p.parse(varargin{:});
 
     if isempty(p.Results.err2)
-        yhi = p.Results.err1;
-        ylo = p.Results.err1;
+        yhi = y + p.Results.err1;
+        ylo = y - p.Results.err1;
     else
         ylo = p.Results.err1;
         yhi = p.Results.err2; 
+    end
+    
+%     function tf = szeq(varargin)
+%         szc = cellfun(@size, varargin{:}, 'UniformOutput', false);
+%         tf = isequal(szc{:});
+%     end
+
+    if isvector(y)
+        y = makecol(y);
+        ylo = makecol(ylo);
+        yhi = makecol(yhi);
+    end
+    
+    if isempty(p.Results.lastX)
+        x = makecol(x);
+        lastX = x(end, :) + x(end, :) - x(end-1, :);
+    else
+        lastX = p.Results.lastX;
+    end
+    
+%     assert(szeq(y,ylo,yhi), 'All inputs must have the same size and shape');
+    if isvector(x)
+        x = makecol(x);
+        assert(numel(x) == size(y, 1), 'X and Y must have same number of rows');
+        assert(isscalar(lastX));
+    else
+        if isscalar(lastX);
+            lastX = repmat(lastX, 1, size(x, 2));
+        end
     end
     
     if isempty(p.Results.color)
@@ -33,9 +68,64 @@ function [hMean, hError] = stairsError(x,y, varargin)
     else
         colorArg = {'Color', p.Results.color};
     end
-
-    hMean = stairs(x, y, colorArg{:}, 'LineWidth', p.Results.lineWidth);
     
+    holdstate = ishold;
+    
+    hMean = stairs(x, y, colorArg{:}, 'LineWidth', p.Results.lineWidth);
+    hold on;
+
     % build polygons for fill 
+    npts = size(x, 1);
+    nlin = size(x, 2);
+    % X Y
+    % 1 H
+    % 2 H
+    % 2 H
+    % 3 H
+    % 3 L
+    % 2 L
+    % 2 L
+    % 1 L
+    
+    % add on a tail value so that the last point gets plotted
+    x = cat(1, x, lastX);
+    y = cat(1, y, y(end, :));
+    ylo = cat(1, ylo, ylo(end, :));
+    yhi = cat(1, yhi, yhi(end, :));
+
+    switch p.Results.errorStyle
+        case 'fill'
+            X = repelem(x, 2, 1);
+            Xud = repelem(flipud(x(2:end-1, :)), 2, 1);
+            X = circshift(cat(1, X, Xud), -1, 1);
+            Y = cat(1, repelem(yhi(1:end-1, :), 2, 1), repelem(flipud(ylo(1:end-1)), 2, 1));
+
+            hError = patch(X, Y, [0 0 0], 'EdgeColor', 'none', 'FaceAlpha', p.Results.errorAlpha);
+            for i = 1:nlin
+                if isempty(p.Results.errorColor)
+                    hError(i).FaceColor = hMean(i).FaceColor;
+                else
+                    hError(i).FaceColor = p.Results.errorColor;
+                end
+            end
+            
+        case 'stairs'
+            hError = stairs(x, ylo, 'LineWidth', p.Results.lineWidth/2);
+            hold on;
+            hError(:, 2) = stairs(x, yhi, 'LineWidth', p.Results.lineWidth/2);
+            
+            for i = 1:nlin
+                if isempty(p.Results.errorColor)
+                    set(hError(i, :), 'Color', hMean(i).Color);
+                else
+                    set(hError(i, :), 'Color', p.Results.errorColor);
+                end
+            end
+    end
+ 
+    if ~holdstate
+        hold off;
+    end
+    uistack(hMean, 'top');
     TrialDataUtilities.Plotting.hideInLegend(hError);
 end
