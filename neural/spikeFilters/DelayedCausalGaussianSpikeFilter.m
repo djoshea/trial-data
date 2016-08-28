6f050372a7d61058b5ec1e54155da90e6d366779
Dan O'Shea <dan@djoshea.com>
2016-08-28 13:29:17 -0700

    Adding DelayedCausal Gaussian filter

---
+++ neural/spikeFilters/DelayedCausalGaussianSpikeFilter.m
@@ -0,0 +1,18 @@
+classdef DelayedCausalGaussianSpikeFilter < GaussianSpikeFilter
+
+    methods
+        function sf = DelayedCausalGaussianSpikeFilter(varargin)
+            sf = sf@GaussianSpikeFilter(varargin{:});
+            sf.binAlignmentMode = SpikeBinAlignmentMode.Causal;
+            sf.delayPeak = sf.halfWidthSigmas * sf.sigma;
+            sf.truncateFuture = 0;
+        end
+    end
+    
+    methods(Access=protected)
+        function str = subclassGetDescription(sf)
+            str = sprintf('%.0f sigma', sf.sigma);
+        end
+    end
+
+end
