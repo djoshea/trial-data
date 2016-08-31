499d8683593c64499542aa700c739e4b0673f3fe
Dan O'Shea <dan@djoshea.com>
2016-08-31 16:23:43 -0700

    Embedding alphabet function in mksort export

--- +TrialDataUtilities/+MKsort/exportForSorting.m
+++ +TrialDataUtilities/+MKsort/exportForSorting.m
@@ -204,6 +204,28 @@ end
 
 return;
 
+end
+
+function alpha = alphabet()
+    % alpha = alphabet()
+    %
+    % Returns the alphabet, caps then lowercase:
+    % ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
+    %
+    % This file is part of the spike sorting software package MKsort, licensed
+    % under GPL version 2.
+    % 
+    % Copyright (C) 2010, 2011, 2012, 2013 The Board of Trustees of The Leland
+    % Stanford Junior University
+    % 
+    % Written by Matt Kaufman
+    % 
+    % Please see mksort.m for full license and contact details.
+
+    alpha = [char('A' + (0:25)) char('a' + (0:25))];
+
+end
+
 % for arr = 1:length(nevNames)
 %   for fileNum = 1:length(nevNames{arr})
 %  
