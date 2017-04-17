d0e21a90f8daaf7c5634f0854fbace1f53998fe3
Dan O'Shea <dan@djoshea.com>
2017-04-17 08:28:41 -0700

    Large speed improvement for spike rate filtering

--- +TrialDataUtilities/+Data/resampleDataCellInTime.m
+++ +TrialDataUtilities/+Data/resampleDataCellInTime.m
@@ -8,6 +8,7 @@ function [dataCell, timeCell] = resampleDataCellInTime(dataCell, timeCell, varar
     p.addParameter('timeReference', 0, @isscalar);
     p.addParameter('binAlignmentMode', BinAlignmentMode.Centered, @(x) isa(x, 'BinAlignmentMode'));
     p.addParameter('resampleMethod', 'filter', @ischar); % valid modes are filter, average, repeat , interp   
+    p.addParameter('uniformlySampled', false, @islogical); % can speed things up if you know it's arleady uniform
     
     % these are used as a secondary guard to truncate data within tMin :
     % tMax, when the input data includes padded edges to facilitate
@@ -26,7 +27,8 @@ function [dataCell, timeCell] = resampleDataCellInTime(dataCell, timeCell, varar
             'timeDelta', p.Results.timeDelta, 'timeReference', p.Results.timeReference, ...
             'binAlignmentMode', p.Results.binAlignmentMode, ...
             'interpolateMethod', p.Results.interpolateMethod, ...
-            'resampleMethod', p.Results.resampleMethod);
+            'resampleMethod', p.Results.resampleMethod, ...
+            'uniformlySampled', p.Results.uniformlySampled);
         
         mask = t >= tMinExcludingPadding(iD) & t <= tMaxExcludingPadding(iD);
         dataCell{iD} = d(mask, :, :, :);
