3d8e60d8bfb16734ae24540ba34babea5cd7d099
Dan O'Shea <dan@djoshea.com>
2017-11-27 15:16:48 -0800

    blendcolors in CIELuv space

--- +TrialDataUtilities/+Color/blendcolors.m
+++ +TrialDataUtilities/+Color/blendcolors.m
@@ -5,6 +5,10 @@ function cmap = blendcolors(colors, n, varargin)
     p.parse(varargin{:});
     interval = p.Results.interval;
     
-    cmap = interp1(linspace(0, 1, size(colors, 1)), colors, linspace(interval(1), interval(2), n));
+    colorLuv = TrialDataUtilities.Color.convert('rgb->Luv', colors);
+    
+    cmapLuv = interp1(linspace(0, 1, size(colors, 1)), colorLuv, linspace(interval(1), interval(2), n));
+    
+    cmap = TrialDataUtilities.Color.convert('Luv->rgb', cmapLuv);
 
 end
