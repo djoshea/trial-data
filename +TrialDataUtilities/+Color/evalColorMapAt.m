8c59578e202b8265ec637c36fdeb7bcec8b63dc9
Dan O'Shea <dan@djoshea.com>
2018-01-25 18:29:04 -0800

    Phase map, phase amplitude map, 2d colormap evaluation

--- +TrialDataUtilities/+Color/evalColorMapAt.m
+++ +TrialDataUtilities/+Color/evalColorMapAt.m
@@ -1,7 +1,12 @@
-function ceval = evalColorMapAt(cmap, at)
+function ceval = evalColorMapAt(cmap, at, clim)
     if isa(cmap, 'function_handle')
         cmap = cmap(1000);
     end
+    if nargin < 3
+        clim = [0 1];
+    end
     N = size(cmap, 1);
-    ceval = interp1((0:N-1) / (N-1), cmap, at);
+    
+    cmapOrigAt = (0:N-1) / (N-1) * (clim(2)-clim(1)) + clim(1);
+    ceval = interp1(cmapOrigAt, cmap, at);
 end
