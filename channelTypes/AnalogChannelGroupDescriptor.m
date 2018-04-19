46d37dee553dd0c449f2c7f142465f2ea49af6ce
Dan O'Shea <dan@djoshea.com>
2018-04-19 00:23:11 +0000

    Adding units spec for individual columns of analog channel group

--- channelTypes/AnalogChannelGroupDescriptor.m
+++ channelTypes/AnalogChannelGroupDescriptor.m
@@ -159,8 +159,11 @@ classdef AnalogChannelGroupDescriptor < ChannelDescriptor
             data = convertAccessDataSingleToMemory@ChannelDescriptor(cd, fieldIdx, data);
         end
         
-        function cdIndividual = buildIndividualSubChannel(cd, name, index)
-            cdIndividual = AnalogChannelDescriptor.buildSharedMatrixColumnAnalog(name, cd.name, index, cd.timeField, cd.unitsByField{1}, cd.unitsByField{2}, ...
+        function cdIndividual = buildIndividualSubChannel(cd, name, index, units)
+            if nargin < 4
+                units = cd.unitsByField{1};
+            end
+            cdIndividual = AnalogChannelDescriptor.buildSharedMatrixColumnAnalog(name, cd.name, index, cd.timeField, units, cd.unitsByField{2}, ...
                 'scaleFromLims', cd.scaleFromLims, 'scaleToLims', cd.scaleToLims, ...
                 'dataClass', cd.originalDataClassByField{1}, 'timeClass', cd.originalDataClassByField{2});
         end
