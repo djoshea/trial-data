a81ad289a9ee7aaa905093dbfb0f79e6e6816681
Dan O'Shea <dan@djoshea.com>
2018-08-23 17:45:12 -0700

    Adding colormaps and blendcolors

--- +TrialDataUtilities/+Color/blendcolors.m
+++ +TrialDataUtilities/+Color/blendcolors.m
@@ -1,11 +1,12 @@
 function cmap = blendcolors(colors, n, varargin)
     p = inputParser();
+    p.addParameter('inputSpace', 'rgb', @ischar);
     p.addParameter('interval', [0 1], @isvector);
     p.KeepUnmatched = false;
     p.parse(varargin{:});
     interval = p.Results.interval;
     
-    colorLuv = TrialDataUtilities.Color.convert('rgb->Luv', colors);
+    colorLuv = TrialDataUtilities.Color.convert(sprintf('%s->Luv', p.Results.inputSpace), colors);
     
     cmapLuv = interp1(linspace(0, 1, size(colors, 1)), colorLuv, linspace(interval(1), interval(2), n));
     
