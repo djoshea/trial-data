dba34656b017599b63b251401291822dd3b57609
Dan O'Shea <dan@djoshea.com>
2019-01-07 20:36:18 -0800

    Sorting npix data by cluster group

--- external/+Neuropixel/addSegmentedDatasetToTrialData.m
+++ external/+Neuropixel/addSegmentedDatasetToTrialData.m
@@ -21,42 +21,55 @@ function td = addSegmentedDatasetToTrialData(td, seg, varargin)
             end
             
             if p.Results.sortByClusterPosition
-                cluster_idx = sortClusters(cluster_mask);
+                cluster_ind = sortClusters(cluster_mask);
             else
-                cluster_idx = find(cluster_mask);
+                cluster_ind = find(cluster_mask);
             end
-            electrodes = seg.cluster_ids(cluster_idx);
+            electrodes = seg.cluster_ids(cluster_ind);
             units = zeros(numel(electrodes), 1);
         
-            data = seg.spike_times_ms_rel_start(:, cluster_idx);
+            data = seg.spike_times_ms_rel_start(:, cluster_ind);
                 
             arrayName = sprintf('%s_%s', p.Results.arrayPrefix, cluster_groups_keep(iG));
             td = td.addSpikeArrayChannel(arrayName, electrodes, units, data, 'isAligned', false, 'raw', true);
+            
+            % add metadata
+            td = td.setChannelMetaKey(arrayName, 'kilosort_pathLeaf', seg.dataset.pathLeaf);
+            td = td.setChannelMetaKey(arrayName, 'cluster_idx', seg.cluster_ids(cluster_ind));
+            td = td.setChannelMetaKey(arrayName, 'cluster_groups', seg.cluster_groups(cluster_ind));
         end
     else
         cluster_mask = ismember(seg.cluster_groups, cluster_groups_keep);
    
         if any(cluster_mask)  
             if p.Results.sortByClusterPosition
-                cluster_idx = sortClusters(cluster_mask);
+                cluster_ind = sortClusters(cluster_mask);
             else
-                cluster_idx = find(cluster_mask);
+                cluster_ind = find(cluster_mask);
             end
-            electrodes = seg.cluster_ids(cluster_idx);
+            electrodes = seg.cluster_ids(cluster_ind);
             units = zeros(numel(electrodes), 1);
 
-            data = seg.spike_times_ms_rel_start(:, cluster_idx);
+            data = seg.spike_times_ms_rel_start(:, cluster_ind);
 
             arrayName = p.Results.arrayPrefix;
             td = td.addSpikeArrayChannel(arrayName, electrodes, units, data, 'isAligned', false, 'raw', true);
+            
+            % add metadata
+            td = td.setChannelMetaKey(arrayName, 'kilosort_pathLeaf', seg.dataset.pathLeaf);
+            td = td.setChannelMetaKey(arrayName, 'cluster_idx', seg.cluster_ids(cluster_ind));
+            td = td.setChannelMetaKey(arrayName, 'cluster_groups', seg.cluster_groups(cluster_ind));
         end
     end
 
     td = td.addOrUpdateBooleanParam(sprintf('%s_hasData', p.Results.arrayPrefix), seg.trial_has_data);
+    
 
 function takeIdx = sortClusters(cluster_mask)
-    y = clusterCOM(cluster_mask, 2);
-    [~, takeIdx] = sort(y, 1, 'descend');
+    takeIdx = find(cluster_mask);
+    y = clusterCOM(takeIdx, 2);
+    [~, sortIdx] = sort(y, 1, 'descend');
+    takeIdx = takeIdx(sortIdx);
 end
 
 end
